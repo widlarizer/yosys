1238fcdb40880c23bda3ce8dc4f9abfa97a1b88c
Emil J. Tywoniak <emil@tywoniak.eu>
2025-10-06 18:50:55 +0200

    Revert "Merge pull request #4997 from YosysHQ/emil/techmap-alu-half-adder"
    This reverts commit a316b8f90c198e44ea0f0d169ca70009541ecef8, reversing
    changes made to d4071b63f733fef584d4db9fe4a57f340c8299fc.

--- techlibs/common/techmap.v
+++ techlibs/common/techmap.v
@@ -283,16 +283,9 @@ module _90_alu (A, B, CI, BI, X, Y, CO);
 	\$pos #(.A_SIGNED(A_SIGNED), .A_WIDTH(A_WIDTH), .Y_WIDTH(Y_WIDTH)) A_conv (.A(A), .Y(A_buf));
 	\$pos #(.A_SIGNED(B_SIGNED), .A_WIDTH(B_WIDTH), .Y_WIDTH(Y_WIDTH)) B_conv (.A(B), .Y(B_buf));
 
-	(* force_downto *)
-	wire [Y_WIDTH-1:0] P;
-	wire [Y_WIDTH-1:0] G;
-	wire [Y_WIDTH-1:0] Cnull;
-	assign Cnull = 1'b0;
+	\$lcu #(.WIDTH(Y_WIDTH)) lcu (.P(X), .G(AA & BB), .CI(CI), .CO(CO));
 
-	\$fa #(.WIDTH(Y_WIDTH)) fa (.A(AA), .B(BB), .C(Cnull), .X(G), .Y(P));
-	\$lcu #(.WIDTH(Y_WIDTH)) lcu (.P(P), .G(G), .CI(CI), .CO(CO));
-
-	assign X = P;
+	assign X = AA ^ BB;
 	assign Y = X ^ {CO, CI};
 endmodule
 
